1694e088ef3a1d7815ffdf35155412514c79903a
Eren <erencoban1997@gmail.com>
2025-03-30 20:03:47 +0300

    object

---
+++ var.js
@@ -0,0 +1,55 @@
+//let num = 0
+
+//console.log( 'num', num);
+
+// const arr = new Array(5);
+// arr.length = 10;
+// arr.fill(10);
+// console.log(arr);
+
+//
+// const arr = [1, 2, 3];
+// for ( const value of arr) {
+//   console.log(value);
+// }
+
+
+
+//arr.splice(1, 2)
+//console.log(typeof arr);
+//arr.push(4);
+//arr.pop();
+//console.log(arr.pop());
+//arr.unshift(4, 5, 6);
+//console.log(arr);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+
+
+// for (let i = 10; i > 0; i--) {
+//     console.log('i =' + i);
+//   for (let j = 0; j < 5; j++){
+//     console.log('j =' + j);
+//   }
+// }
+
+// const fullname = 'Eren Coban';
+// const city = 'Ankara';
+// const yearOfBirth = '1997';
+//
+// var all = `Benim adim ${fullname}. ${city} yasiyorum. ${(2025-yearOfBirth<=18)? '18 yasinin atindasiniz': '18 yasinin ustundeyim'} yasindayim.`
+//
+// console.log(all);
+
+let person = {
+  name : 'eren',
+  lastname : 'COBAN',
+  age : 28 ,
+  hobbies : ['bisiklet', 'boardgame', 'heartstone'],
+}
+
+console.log(person);
+console.log(typeof person);
