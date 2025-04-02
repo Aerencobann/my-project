5dd45caa8da876e51f5a799c25684c6efad67156
Eren <erencoban1997@gmail.com>
2025-04-02 15:28:07 +0300

    function derlerine devam

--- var.js
+++ var.js
@@ -82,6 +82,30 @@
 // })
 
 
+//Function (fonksiyonlar )
+
+
+
+// function karesinial(sayi){
+//         return sayi*sayi;
+// }
+//
+// let a = karesinial(2); // 4 olarak dondurecek
+// let b = karesinial(4); // 16 olarak dondurecek
+//
+// console.log(a);
+// console.log(b);
+
+
+// function toplam(sayi1, sayi2){
+//   function karesinial(x){
+//     return x*x;
+//   }
+//   return karesinial(sayi1) + karesinial(sayi2);
+// }
+//
+// let sonuc = toplam(2,4);
+// console.log(sonuc);
 
 
 
